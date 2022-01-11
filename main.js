e6728a1ed1e18dda2780455f637d682b4668e0ac
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-11 13:10:49 +0100

    Track added as a comment

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito,
+il nostro script JS in cui:
+Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post,
+numero progressivo da 1 a n nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es.
+ Unsplash (https://unsplash.it/300/300?image=<id> */
